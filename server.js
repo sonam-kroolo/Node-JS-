e4f3ad7886b3f3d1b55916534f3f38b1420fed91
sonamKroolo <sonam.charak@kroolo.com>
2025-05-31 17:52:02 +0530

    loading files

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import http from "http";
 const PORT = process.env.PORT;
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
   // res.write("12345566");
 
   // res.setHeader("content-type", "text/html");
@@ -14,16 +22,24 @@ const server = http.createServer((req, res) => {
 
   try {
     if (req.method === "GET") {
+      let filePath;
       if (req.url === "/") {
-        res.writeHead(200, { "constent-type": "text/html" });
-        res.end("<h1>HomePage</h1>");
+        // res.writeHead(200, { "constent-type": "text/html" });
+        // res.end("<h1>HomePage</h1>");
+        filePath = path.join(__dirname, "public", "index.html");
       } else if (req.url === "/about") {
-        res.writeHead(200, { "constent-type": "text/html" });
-        res.end("<h1>About page</h1>");
+        // res.writeHead(200, { "constent-type": "text/html" });
+        // res.end("<h1>About page</h1>");
+        filePath = path.join(__dirname, "public", "about.html");
       } else {
-        res.writeHead(404, { "constent-type": "text/html" });
-        res.end("<h1>Not Found</h1>");
+        // res.writeHead(404, { "constent-type": "text/html" });
+        // res.end("<h1>Not Found</h1>");
+        throw new Error("Not found");
       }
+      const data = await fs.readFile(filePath);
+      res.setHeader("content-type", "text/html");
+      res.write(data);
+      res.end();
     } else {
       throw new Error("Method not found");
     }
