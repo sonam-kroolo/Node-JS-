242c87c8e757a6d2c2ac2634f131868eb7a0dbb1
sonamKroolo <sonam.charak@kroolo.com>
2025-05-31 18:44:53 +0530

    find user by id

--- server2.js
+++ server2.js
@@ -12,6 +12,25 @@ const server = createServer((req, res) => {
     res.setHeader("content-type", "application/json");
     res.write(JSON.stringify(users));
     res.end();
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+    const id = req.url.split("/")[3];
+    const user = users.find((user) => user.id === parseInt(id));
+    if (user) {
+      res.setHeader("content-type", "application/json");
+      res.write(JSON.stringify(user));
+      res.end();
+    } else {
+      res.setHeader("content-type", "application/json");
+      res.statusCode = 404;
+      res.write(JSON.stringify({ message: "User Not found" }));
+      res.end();
+    }
+
+    // res.setHeader("content-type", "application/json");
+    // res.write(JSON.stringify({ id: 1, name: "john Doe" }));
+    // res.end();
+
+    // res.write(JSON.stringify(users));
   } else {
     res.setHeader("content-type", "application/json");
     res.statusCode = 404;
